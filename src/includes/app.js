bd687255c68efff5fcb379896274e30ad48061d0
BillyCWood <billyc.wood4@gmail.com>
2024-06-06 23:02:29 -0600

    Removed dead php code.
    Renamed 'vecavision.js' to 'app.js'
    
    Added snap scrolling on home page.

---
+++ src/includes/app.js
@@ -0,0 +1,20 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+function mobileHamburgerBars(){
+    var topNav = document.getElementById("responsive__container");
+    if (topNav.className === "responsive__container") {
+      topNav.className += " active";
+    } else {topNav.className = "responsive__container";}
+
+}
--- src/includes/vecavision.js
+++
@@ -1,7 +0,0 @@
-function mobileHamburgerBars(){
-    var topNav = document.getElementById("responsive__container");
-    if (topNav.className === "responsive__container") {
-      topNav.className += " active";
-    } else {topNav.className = "responsive__container";}
-
-}
